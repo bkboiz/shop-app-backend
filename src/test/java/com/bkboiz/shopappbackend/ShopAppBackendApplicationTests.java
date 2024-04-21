44dc6126f32c772ed76ffac2a163c0e0477f42c0
bkboiz <Standbyme@93>
2024-04-21 20:55:04 +0700

    first commit

---
+++ src/test/java/com/bkboiz/shopappbackend/ShopAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkboiz.shopappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
